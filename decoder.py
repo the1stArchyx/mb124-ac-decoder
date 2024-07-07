85418fdb7a5390fd5633dec7047fc6d1a4889241
Lauri Lindholm <archyx@pp.htv.fi>
2024-07-07 12:24:33 +0300

    License notice (finally) attached to the source file itself

--- decoder.py
+++ decoder.py
@@ -1,5 +1,24 @@
 #!/bin/python3
 
+# decoder.py - datastream decoder for Mercedes-Benz 124 A/C (SA 580) diagnostics
+
+#    Copyright (C) 2023-2024  Lauri "Archyx" Lindholm
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# ------------------------------------------------------------------------------
+
 # TODO:
 
 #   - Group user inputs together:
